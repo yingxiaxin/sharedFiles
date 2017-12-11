203faa7456eb5bbf0790dec463ed31dc1dc23f12
yingxiaxin <yingxiaxin@163.com>
2017-12-11 17:36:23 +0800

    1

---
+++ 共享文件/douDiZhu/client/js/Card.js
@@ -0,0 +1,84 @@
+// import CardMapping from './CardMapping';
+
+class Card
+{
+    constructor(num, type, parentCon)
+    {
+        this.cardNum = num;
+        this.cardType = type;
+        this.parentContainer = parentCon;
+
+        this.color = CardMapping.typeMap.get(type).color;
+        this.flower = CardMapping.typeMap.get(type).code;
+
+        //纸牌的最外层容器
+        let instanceDiv = document.createElement('div');
+        instanceDiv.style.position = 'absolute';
+        instanceDiv.style.cursor = 'pointer';
+        instanceDiv.style.userSelect = 'none';
+
+        //纸牌左上角的数字
+        let numTag1 = document.createElement('span');
+        numTag1.innerText = this.cardNum;
+        numTag1.style.position = 'absolute';
+        numTag1.style.top = '8%';
+        numTag1.style.left = '10%';
+        numTag1.style.color = this.color;
+
+        //纸牌右下角的数字
+        let numTag2 = document.createElement('span');
+        numTag2.innerText = this.cardNum;
+        numTag2.style.position = 'absolute';
+        numTag2.style.bottom = '8%';
+        numTag2.style.right = '10%';
+        numTag2.style.transform = 'rotate(180deg)';
+        numTag2.style.color = this.color;
+
+        //纸牌左上角的花色
+        let typeTag1 = document.createElement('span');
+        typeTag1.innerHTML = this.flower;
+        typeTag1.style.position = 'absolute';
+        typeTag1.style.top = '18%';
+        typeTag1.style.left = '10%';
+        typeTag1.style.color = this.color;
+
+        //纸牌右下角的花色
+        let typeTag2 = document.createElement('span');
+        typeTag2.innerHTML = this.flower;
+        typeTag2.style.position = 'absolute';
+        typeTag2.style.bottom = '18%';
+        typeTag2.style.right = '10%';
+        typeTag2.style.transform = 'rotate(180deg)';
+        typeTag2.style.color = this.color;
+
+        //纸牌中间的花色
+        let midTag = document.createElement('i');
+        midTag.innerHTML = this.flower;
+        midTag.style.position = 'absolute';
+        midTag.style.top = '50%';
+        midTag.style.left = '50%';
+        midTag.style.transform = 'translate(-50%, -50%)';
+        midTag.style.color = this.color;
+
+        //将数字和花色符号加入纸牌容器
+        instanceDiv.appendChild(numTag1);
+        instanceDiv.appendChild(numTag2);
+
+        instanceDiv.appendChild(typeTag1);
+        instanceDiv.appendChild(typeTag2);
+
+        instanceDiv.appendChild(midTag);
+        
+        instanceDiv.setAttribute('class', 'transparentContainer');
+
+        if(this.parentContainer != null && typeof(this.parentContainer) != 'undefined')
+        {
+            this.parentContainer.appendChild(instanceDiv);
+        }
+
+        this.instance = instanceDiv;
+        return this.instance;
+    }
+}
+
+// export default Card;
---
+++ 共享文件/douDiZhu/client/js/CardMapping.js
@@ -0,0 +1,38 @@
+class CardMapping
+{
+
+}
+
+CardMapping.typeMap = new Map();
+let hongtao = 
+{
+    color: 'palevioletred',
+    code: '&#9829',
+    index: '0'
+};
+
+let fangkuai = 
+{
+    color: 'orangered',
+    code: '&#9830',
+    index: '1'
+};
+
+let heitao = 
+{
+    color: 'black',
+    code: '&#9824',
+    index: '2'
+};
+
+let meihua = 
+{
+    color: 'grey',
+    code: '&#9827',
+    index: '3'
+};
+
+CardMapping.typeMap.set('0', hongtao);
+CardMapping.typeMap.set('1', fangkuai);
+CardMapping.typeMap.set('2', heitao);
+CardMapping.typeMap.set('3', meihua);
