68805493a5a4ab3aedae825a3383c8495d1f37c2
yingxiaxin <yingxiaxin@163.com>
2018-01-08 17:17:47 +0800

    1

--- 共享文件/douDiZhu/client/js/global.js
+++ 共享文件/douDiZhu/client/js/global.js
@@ -26,7 +26,7 @@ window.G.init = function()
     G.CONTAINER_CLICK = 'auto';     //是否在某块区域上禁止鼠标操作
     G.CONTAINER_NO_CLICK = 'none';
 
-    G.DEALCARD_RATE = 100;  //发牌的快慢速率，默认100毫秒
+    G.DEALCARD_RATE = 60;  //发牌的快慢速率，默认100毫秒
     G.OVERLAP_FACTOR = 0.7;    //玩家牌不重叠的宽度比例
     G.OVERLAP_FACTOR_TTB = 0.07;
 
